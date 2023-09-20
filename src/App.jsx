9a68afdc50dda2e149849c908cec71aa5b789672
PollitoAng <steveang880@gmail.com>
2023-09-20 01:45:19 -0600

    Ultima actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,66 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [showButtons, setShowButtons] = useState(false);
+  const [showPasswordConfirmation, setShowPasswordConfirmation] = useState(false);
 
   const showAdditionalButtons = () => {
     setShowButtons(true);
   };
+
+  const handleEmailInputChange = () => {
+    setShowPasswordConfirmation(true);
+  };
+
+
+  function analizarContraseña() {
+  
+    const contraseña = document.getElementById('passwordUser').value;
+  
+    let letras = 0;
+    let numeros = 0;
+    let simbolos = 0;
+
+    for (let i = 0; i < contraseña.length; i++) {
+      const caracter = contraseña.charAt(i);
+  
+      if (/^[a-zA-Z]+$/.test(caracter)) {
+        letras++;
+      }
+      else if (/^\d+$/.test(caracter)) {
+        numeros++;
+      }
+      else {
+        simbolos++;
+      }
+    }
+  
+    const tamaño = contraseña.length;
+  
+    console.log(`Letras: ${letras}`);
+    console.log(`Números: ${numeros}`);
+    console.log(`Símbolos: ${simbolos}`);
+    console.log(`Tamaño total: ${tamaño}`);
+  }
+  
+  useEffect(() => {
+    const passwordInput = document.getElementById('passwordUser');
+    const emailInput = document.getElementById('emailUser');
+
+    passwordInput.addEventListener('input', analizarContraseña);
+    emailInput.addEventListener('input', handleEmailInputChange);
+
+    return () => {
+      passwordInput.removeEventListener('input', analizarContraseña);
+      emailInput.removeEventListener('input', handleEmailInputChange);
+    };
+  }, []);
+
+
   return (
     <>
-      <div class="header">
+      <div className="header">
         <img src="group-363.png" className="ladrillos" alt="100ladrillos logo" />
         <div className='headerButtonsDiv'>
           {showButtons ? (
@@ -34,15 +85,35 @@ function App() {
         </span>
         <div className='input-div'>
           <label className='label'>Ingresa tu correo electrónico</label>
-          <input placeholder='tu@email.com' className='ingresar-email'></input>
+          <input placeholder='tu@email.com' className='ingresar-email' id='emailUser'></input>
         </div>
         <div className='input-div'>
           <label className='label-2'>Ingresa tu contraseña</label>
-          <input placeholder='Contraseña' className='ingresar-constrasena' type='password'></input>
+          <input placeholder='Contraseña' className='ingresar-constrasena' type='password' id='passwordUser'></input>
           <button className='visibility-off-image'><img src='VisibilityOff.png' /></button>
         </div>
-        <div className='linea-separacion1'></div>
-        <div className='linea-separacion2'></div>
+        <div>
+        {showPasswordConfirmation ? (
+          <>
+            
+          </>
+        ) : (
+          <>
+            <div className='linea-separacion1'></div>
+            <div className='linea-separacion2'></div>
+            <span className='o-registrarte-con'>O registrate con: </span>
+            <div className='botones-inicio-de-sesion-div'>
+              <button className='boton-inicio-de-sesion-google'><img src='2000-px-google-g-logo-1.png'/></button>
+              <button className='boton-inicio-de-sesion-microsoft'><img src='microsoft-windows-logo-1.png'/></button>
+              <button className='boton-inicio-de-sesion-facebook'><img src='facebok-1.png'/></button>
+              <img src='systems-icons.png' className='systems-icons-1'></img>
+              <img src='systems-icons2.png' className='systems-icons-2'></img>
+              <img src='systems-icons2.png' className='systems-icons-3'></img>
+              <img src='systems-icons2.png' className='systems-icons-4'></img>
+            </div>
+          </>
+        )}
+        </div>
       </div>
     </>
   )
